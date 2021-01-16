cf5840f7b5f755775c6fe0fe48273abe1d8a120f
onart <onart97@naver.com>
2021-01-16 18:11:47 +0900

    초기버전 완성

--- case1/case1_2.py
+++ case1/case1_2.py
@@ -18,13 +18,16 @@ VOW=(
 
 # 모든 받침의 공통 위치, 테스트 결과에 따라 수정 가능
 CONSB=(
-    (266,-63,800,240),
+    (300,-63,800,250),
 )
 
 BASEB=(
-    (140,280,980,870),
+    (140,240,980,870),
 )
 
+STD_C=(120,90,820,620)
+STD_VV=(320,-86,715,820)
+STD_HV=(54,136,962,642)
 COMPL=(84.0, -113, 989.0, 849.0)
 
 RATIO=8.4
@@ -55,11 +58,11 @@ def transV(rect1, rect2):
     mat=None
     if rect1[2]-rect1[0]>rect1[3]-rect1[1]:
         mat=psMat.scale(dst[0]/(rect1[2]-rect1[0]))
-        #tr1=psMat.translate(0,(dst[1]+rect1[3]-rect1[1])/2)
+        #tr1=psMat.translate(0,(dst[1]+(rect1[3]-rect1[1])*dst[0]/(rect1[2]-rect1[0]))/2)
         #mat=psMat.compose(mat,tr1)
     else:
         mat=psMat.scale(dst[1]/(rect1[3]-rect1[1]))
-        #tr1=psMat.translate((dst[0]+rect1[2]-rect1[0])/2,0)
+        #tr1=psMat.translate((dst[0]+(rect1[2]-rect1[0])*dst[1]/(rect1[3]-rect1[1]))/2,0)
         #mat=psMat.compose(mat,tr1)
     tr2=psMat.translate(rect2[0]-rect1[0],rect2[1]-rect1[1])
     mat=psMat.compose(mat,tr2)
@@ -67,8 +70,20 @@ def transV(rect1, rect2):
 
 font=fontforge.fonts()[0]
 
+for umso in 'ㄱㄲㄳㄴㄵㄶㄷㄸㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅃㅄㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ':
+    hr=font[umso].boundingBox()
+    font[umso].transform(transC(hr,STD_C))
+for umso in 'ㅏㅐㅑㅒㅓㅔㅕㅖㅣ':
+    mr=font[umso].boundingBox()
+    font[umso].transform(transV(mr,STD_VV))
+for umso in 'ㅗㅛㅜㅠㅡ':
+    mr=font[umso].boundingBox()
+    font[umso].transform(transV(mr,STD_HV))
+for umso in 'ㅘㅙㅚㅝㅞㅟㅢ':   # 조합하여 만듦
+    mr=font[umso].boundingBox()
+    font[umso].transform(transC(mr,COMPL))
+
 for h in HD:
-    headRect=font[h].boundingBox()
     for m in MD:
         midRect=font[m].boundingBox()
         base=plusHan(h,m)
@@ -79,11 +94,13 @@ for h in HD:
             ix=1
         else:
             ix=2
-        font[base].addReference(h,transC(headRect,CONS[ix]))
+        font[base].addReference(h,transC(STD_C,CONS[ix]))
         font[base].addReference(m,transV(midRect,VOW[ix]))
         font[base].transform(transC(font[base].boundingBox(),COMPL))
         for e in ED[1:]:
             term=plusHan(h,m,e)
-            edRect=font[e].boundingBox()
             font[term].addReference(base, transC(COMPL,BASEB[0]))
-            font[term].addReference(e,transC(edRect,CONSB[0]))
+            font[term].addReference(e,transC(STD_C,CONSB[0]))
+            font[term].transform(transC(font[term].boundingBox(),COMPL))
+
+# 확인된 문제: 굵기 보정 사항 없음, ㅘ ㅚ 등 가로+세로 모음 균형이 안 맞음, 자음과 가로모음 일부 겹침
